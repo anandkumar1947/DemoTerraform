4f10c4a0e4c20f0495af7f9df12a81f819657b0a
Anand Kumar Gupta <46086296+anandkumar1947@users.noreply.github.com>
2024-05-02 00:31:46 +0530

    mc

--- demo.tf
+++ demo.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "allow_tls" {
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = ["${data.terraform_remote_state.eip.outputs.eip_addr}/32"]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
